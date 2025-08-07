45592305d80c2b927ff6e7a30470a57e0f330687
David4-max <davidgerhardt11@gmail.com>
2025-08-07 20:30:17 +0200

    Final fix for all known errors

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { OpenAI } = require('openai');
 const dotenv = require('dotenv');
 const fs = require('fs');
-const cors = require('cors'); 
+const cors = require('cors');
 const app = express();
 const port = 3000;
 
